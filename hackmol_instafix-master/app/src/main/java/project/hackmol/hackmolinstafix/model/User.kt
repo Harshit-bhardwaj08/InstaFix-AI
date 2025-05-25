cf685866356e46a426707bb53a2db653cc004418
Harshit-bhardwaj08 <harshitbhardwaj845@gmail.com>
2025-05-25 18:58:33 +0530

    Delete hackmol_instafix-master directory

--- hackmol_instafix-master/app/src/main/java/project/hackmol/hackmolinstafix/model/User.kt
+++
@@ -1,9 +0,0 @@
-package project.hackmol.hackmolinstafix.model
-
-data class User(
-    val id: String = "",
-    val name: String = "",
-    val email: String = "",
-    val phoneNumber: String = "",
-    val isRepairTechnician: Boolean = false
-)
--- hackmol_instafix-master/app/src/main/java/project/hackmol/hackmolinstafix/util/Resource.kt
+++
@@ -1,8 +0,0 @@
-package project.hackmol.hackmolinstafix.util
-
-// A generic class for handling network responses
-sealed class Resource<out T> {
-    data class Success<out T>(val data: T) : Resource<T>()
-    data class Error(val message: String) : Resource<Nothing>()
-    object Loading : Resource<Nothing>()
-}
