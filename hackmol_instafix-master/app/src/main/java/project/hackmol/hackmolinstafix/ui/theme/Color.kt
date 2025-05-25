20bf2e1c22e421e66c0b26570b1a6646fd8b4c42
Harshit-bhardwaj08 <harshitbhardwaj845@gmail.com>
2025-05-25 18:56:10 +0530

    Initial commit of InstaFix project

---
+++ hackmol_instafix-master/app/src/main/java/project/hackmol/hackmolinstafix/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package project.hackmol.hackmolinstafix.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val primaryColor = Color(0xFF8B5CF6) // Purple
+val secondaryColor = Color(0xFFA78BFA) // Light Purple
+val backgroundColor = Color(0xFFF3F4F6) // Light Gray background
+val textColor = Color(0xFF1F2937) // Dark text
+val subtextColor = Color(0xFF6B7280) // Gray text
+val cardColor = Color.White
